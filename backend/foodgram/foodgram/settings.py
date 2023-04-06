3abcde62d5478ca4db1d187dcfbe66ad123548bd
Владислав Дронов <vdronovvladislav@gmail.com>
2023-04-06 22:50:15 +0300

    Добавлен файл setup.cfg и настройки в нем

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'api',
     'users',
     'recipe',
-    
 ]
 
 MIDDLEWARE = [
--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -8,4 +8,5 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('username', 'email')
     empty_value_display = '-пусто-'
 
+
 admin.site.register(User, UserAdmin)
